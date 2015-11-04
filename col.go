47cc7a463726b76e596384d0378e8eacf1635c2b
mandolyte <cecil.new@gmail.com>
2015-11-04 07:20:53 -0500

    Misc changes, refactor

---
+++ col.go
@@ -0,0 +1,30 @@
+package sudoku
+import "fmt"
+
+type col struct {
+  colnum int
+}
+
+func Newcol(colnum int) col {
+  var c col
+  c.colnum = colnum
+  return c
+}
+
+func (c col) Print() {
+  var i int
+  for i=0; i < 9; i++ {
+    fmt.Print(puzzle[i][c.colnum][0])
+  }
+  fmt.Print("\n")
+}
+
+func (c colnum) is_in_col(candidate int) bool {
+  var i int
+  for i=0; i < 9; i++ {
+    if puzzle[i][c.colnum][0] == candidate {
+      return true
+    }
+  }
+  return false
+}
---
+++ row.go
@@ -0,0 +1,30 @@
+package sudoku
+import "fmt"
+
+type row struct {
+  rownum int
+}
+
+func NewRow(rownum int) row {
+  var r row
+  r.rownum = rownum
+  return r
+}
+
+func (r row) Print() {
+  var j int
+  for j=0; j < 9; j++ {
+    fmt.Print(puzzle[rownum][j][0])
+  }
+  fmt.Print("\n")
+}
+
+func (r rownum) is_in_row(candidate int) bool {
+  var j int
+  for j=0; j < 9; j++ {
+    if puzzle[r.rownum][j][0] == candidate {
+      return true
+    }
+  }
+  return false
+}
