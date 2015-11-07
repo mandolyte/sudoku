c332937800b0985038186184894f07cf088fb2e4
mandolyte <cecil.new@gmail.com>
2015-11-07 12:24:45 -0500

    added misc with init() and removed process.go

---
+++ misc.go
@@ -0,0 +1,20 @@
+package sudoku
+
+// misc
+
+var squares [9][3]int
+
+func init() {
+  // set the corners of each square in the puzzle
+  squares[0] = [3]int{0,0,0}
+  squares[1] = [3]int{0,3,0}
+  squares[2] = [3]int{0,6,0}
+  squares[3] = [3]int{3,0,0}
+  squares[4] = [3]int{3,3,0}
+  squares[5] = [3]int{3,6,0}
+  squares[6] = [3]int{6,0,0}
+  squares[7] = [3]int{6,3,0}
+  squares[8] = [3]int{6,6,0}
+}
+
+
